07d4dfd1dbf906c802e056df347f6866397b6fa4
vimanew <kethmi12345@gmail.com>
2024-09-08 21:12:19 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -1,20 +1,17 @@
-const { readEnv } = require('../lib/database');
-const { cmd, commands } = require('../command');
-const os = require('os');
-const { runtime } = require('../lib/functions');
-const { MessageType } = require('@adiwajshing/baileys');
+const config = require('../config')
+const {cmd, commands} = require('../command')
+const os = require("os")
+const {runtime} = require('../lib/functions')
 
 cmd({
     pattern: "menu",
-    alias: ["panel", "penal", "list", "allmenu"],
+    alias: ["panel","penal","list","allmenu"],
     react: "🪴",
     desc: "Check menu all",
     category: "main",
     filename: __filename
-}, async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+}, async (conn, mek, m, {from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
     try {
-        const config = await readEnv();
-
         // RAM usage
         const totalRAM = Math.round(require('os').totalmem() / 1024 / 1024); // Total RAM in MB
         const usedRAM = (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2); // Used RAM in MB
@@ -44,56 +41,141 @@ cmd({
  4 💥𝐎𝐓𝐇𝐄𝐑 𝐌𝐄𝐍𝐔⤵💥
 
 
-*✸ℤ𝔸𝕀ℝ𝕆 𝕄𝔻 𝔹𝕆𝕋✸*`;
+*✸ℤ𝔸𝕀ℝ𝕆 𝕄𝔻 𝔹𝕆𝕋✸*`
+
+        // URL of the image you want to include
+        const imageUrl = 'https://i.ibb.co/6mzcHsN/20240907-102239.jpg'; // Replace with your actual image URL
 
         // Send the image with the status as the caption
         const sentMsg = await conn.sendMessage(from, {
-            image: { url: 'https://i.ibb.co/6mzcHsN/20240907-102239.jpg' }, // Replace with your image URL
-            caption: status,
-            buttons: [
-                { buttonId: '1', buttonText: { displayText: '1 💥𝐎𝐖𝐍𝐄𝐑 𝐌𝐄𝐍𝐔⤵' }, type: 1 },
-                { buttonId: '2', buttonText: { displayText: '2 💥𝐆𝐑𝐎𝐔𝐏 𝐌𝐄𝐍𝐔⤵' }, type: 1 },
-                { buttonId: '3', buttonText: { displayText: '3 💥𝐃𝐎𝐖𝐍𝐋𝐎𝐀𝐃 𝐌𝐄𝐍𝐔⤵' }, type: 1 },
-                { buttonId: '4', buttonText: { displayText: '4 💥𝐎𝐓𝐇𝐄𝐑 𝐌𝐄𝐍𝐔⤵' }, type: 1 }
-            ],
-            headerType: 1
+            image: { url: imageUrl },
+            caption: status
         }, { quoted: mek || null });
-
+        
+        
+        
+    //    5💥💥💥💥5💥💥
+        
+        
         conn.ev.on('messages.upsert', async (msgUpdate) => {
             const msg = msgUpdate.messages[0];
-            if (!msg.message || !msg.message.buttonsResponseMessage) return;
-            const selectedOption = msg.message.buttonsResponseMessage.selectedButtonId;
 
-            if (msg.message.buttonsResponseMessage.contextInfo && msg.message.buttonsResponseMessage.contextInfo.stanzaId === sentMsg.key.id) {
+            if (msg.message && msg.message.extendedTextMessage &&
+                msg.message.extendedTextMessage.contextInfo.stanzaId === sentMsg.key.id) {
+
+                const selectedOption = msg.message.extendedTextMessage.text.trim().toLowerCase();
+
+
+    //    conn.ev.on('messages.upsert', async (msgUpdate) => {
+    //        const msg = msgUpdate.messages[0];
+      //      if (!msg.message || !msg.message.extendedTextMessage) return;
+          //  const selectedOption = msg.message.extendedTextMessage.text.trim();
+            
+      //      if (msg.message.extendedTextMessage.contextInfo && msg.message.extendedTextMessage.contextInfo.stanzaId === sentMsg.key.id) {
                 switch (selectedOption) {
                     case '1':
-                        await conn.sendMessage(from, { text: "✔️ Option 1 selected. Opening URL..." });
-                        // Open URL
-                        await conn.sendMessage(from, { text: "www.google.com" });
+                        reply(`✸ℤ𝔸𝕀ℝ𝕆 𝕄𝔻 𝔹𝕆𝕋✸ 𝐀𝐈 𝐒𝐘𝐒𝐓𝐄𝐌*⤵*`);
                         break;
                     case '2':
-                        await conn.sendMessage(from, { text: "✔️ Option 2 selected. Opening URL..." });
-                        // Open URL
-                        await conn.sendMessage(from, { text: "www.google.com" });
+                        reply(`💥𝐆𝐑𝐎𝐔𝐏 𝐌𝐄𝐍𝐔
+> *_.add_*
+Ex.( .add 94776734030 )
+ගෲපයේ නැති සමාජිකයෙක් Add කරයි.
+
+> *_.promote_*
+ Ex.( .promote @mention member )
+ගෲපයේ සමාජිකයෙක් Admin බල තල ලබාදෙයි.✅
+
+> *_.demote_*
+ Ex.( .demote @mention member )
+ගෲපයේ සමාජිකයෙක් Admin බල තල ඉවත් කරයි.✅
+
+> *_.setabout_*
+ Ex.( .setabout ZAIRO MD )
+ගෲපයේ ඇති Bio එක වෙනස් කරයි.✅.
+
+> *_.setname_*
+ Ex.( .setname ZAIRO MD )
+ගෲපයේ ඇති නම වෙනස් කරයි.✅.
+
+> *_.tagall_*
+Ex.( .tagall )
+ගෲපයේ සියලුම සමාජිකකයන් එක වර ටැග් කරයි.✅
+
+> *_.grouplink_*
+Ex.( .grouplink )
+ඔබ සිටින ගෲපයේ ලින්ක් එක ලබා දෙයි.✅
+
+> *_.mute_*
+Ex.( .mute )
+ගෲපය වසා දමයි.✅ 
+
+> *_.unmute_*
+Ex.( .upmute )
+ගෲපය නැවත Open කරයි✅ 
+
+> *_.kick_*
+Ex.( .kick @9477xxxxxxx )
+ගෲපය Tag කරන සාමාජිකයා ඉවත් කරයි.✅ 
+
+> *_.kickall_*
+Ex.( .kickall )
+ගෲපයේ සියලුම සාමාජිකයන් එකවර ඉවත් කරයි.✅ 
+
+> *_.endgroup_*
+Ex.( .endgroup )
+ගෲපය අවසාන කර දමයි.✅ 
+
+> *_.delall_*
+Ex.( .delall )
+ගෲපයේ සියලුම මැසේජ් මකා දමයි.✅ 
+
+> *_.gjid_*
+Ex.( .gjid )
+ගෲපයේ ලිපිනය ලබා දෙයි.✅ 
+
+> *_.left_*
+Ex.( .left )
+ගෲපයෙන් ඔබ ඉවත් වෙයි.✅ `);
                         break;
                     case '3':
-                        await conn.sendMessage(from, { text: "✔️ Option 3 selected. Opening URL..." });
-                        // Open URL
-                        await conn.sendMessage(from, { text: "www.google.com" });
+                        reply(`💥𝐃𝐎𝐖𝐍𝐋𝐎𝐀𝐃 𝐌𝐄𝐍𝐔
+> *_.song_*
+Ex.( .song <Type Song Name> )
+ඔබ ටයිප් කරන ගීතය බාගත⬇️ කරයි.✅
+
+> *_.video_*
+Ex.( .video <Type Video Name> )
+ඔබ ටයිප් කරන විඩියෝව බාගත⬇️ කරයි.✅
+
+> *_.fb_*
+Ex.( .fb <Type fb video link> )
+ඔබ ටයිප් කරන ගීතය බාගත⬇️ කරයි.✅
+
+> *_.tiktok_*
+Ex.( .tiktok <Type Tiktok Link> )
+ඔබ ටයිප් කරන ටික්ටොක් විඩියෝව බාගත⬇️ කරයි.✅
+
+> *_.mediafire_*
+Ex. ( .mediafire <Enter media fire link> )
+ඔබගේ Mediafire File එක බාගත⬇️ කරයි✅
+
+> *_.gdrive_*
+Ex. ( .gdrive <Enter Google Drive Link> )
+Google drive File එක බාගත⬇️ කරයි✅`);
                         break;
                     case '4':
-                        await conn.sendMessage(from, { text: "✔️ Option 4 selected. Opening URL..." });
-                        // Open URL
-                        await conn.sendMessage(from, { text: "www.google.com" });
+                        reply(`💥𝐎𝐓𝐇𝐄𝐑 𝐌𝐄𝐍𝐔`);
                         break;
                     default:
-                        await conn.sendMessage(from, { text: "Invalid option. Please select a valid menu option (1-4)." });
+                        reply("Invalid option. Please select a valid menu option (1-4).");
                 }
             }
         });
+        
 
     } catch (e) {
-        console.log(e);
-        reply(`Error: ${e}`);
+        console.log(e)
+        reply(`Error: ${e}`)
     }
 });
