5e248aad4107ee4dea72bc842a7d36af3a8bff75
vimanew <kethmi12345@gmail.com>
2024-09-08 16:52:15 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -1,5 +1,5 @@
 
-const {readEnv} = require('../lib/database)
+const {readEnv} = require('../lib/database')
 const {cmd, commands} = require('../command')
 const os = require("os")
 const {runtime} = require('../lib/functions')
