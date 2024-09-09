67b37eb570c71327e52021fa71c437a4efbe9510
vimanew <kethmi12345@gmail.com>
2024-09-10 01:43:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -171,7 +171,7 @@ conn.sendFileUrl = async (jid, url, caption, quoted, options = {}) => {
 //============================================================================ 
 
 
-/*const events = require('./command')
+const events = require('./command')
 const cmdName = isCmd ? body.slice(1).trim().split(" ")[0].toLowerCase() : false;
 if (isCmd) {
 const cmd = events.commands.find((cmd) => cmd.pattern === (cmdName)) || events.commands.find((cmd) => cmd.alias && cmd.alias.includes(cmdName))
@@ -200,9 +200,9 @@ command.on === "sticker" &&
 mek.type === "stickerMessage"
 ) {
 command.function(conn, mek, m,{from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply})
-}}); */
+}}); 
 
-const events = require('./command');
+/* const events = require('./command');
 
 // Get command name
 const cmdName = isCmd ? body.slice(1).trim().split(" ")[0].toLowerCase() : false;
@@ -307,7 +307,7 @@ events.commands.map(async (command) => {
             }, 5000);
         }
     }
-});
+}); */
 
 if(!isOwner && config.MODE === "private") return
 if(!isOwner && isGroup && config.MODE === "inbox") return
