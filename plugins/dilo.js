5dcc35a33cae97fed0fc955a8d16c57c1f02cf6a
vimanew <kethmi12345@gmail.com>
2024-09-10 01:30:05 +0530

    Update dilo.js

--- plugins/dilo.js
+++ plugins/dilo.js
@@ -7,14 +7,6 @@ cmd({
     category: "main",
     filename: __filename
 }, 
-
-// Auto-delete the message after 5 seconds for everyone
-        setTimeout(async () => {
-            await conn.sendMessage(from, {
-                delete: { id: msg.key.id, remoteJid: from, fromMe: false }
-            });
-        }, 5000) // 5000 milliseconds = 5 seconds
-
 async (conn, mek, m, { 
     from, quoted, body, isCmd, command, args, q, isGroup, 
     sender, senderNumber, botNumber2, botNumber, pushname, 
