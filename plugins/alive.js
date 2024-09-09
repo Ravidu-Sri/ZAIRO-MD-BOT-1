5f61f6d0de37db8a08ec0d52caf92f5163d0c113
vimanew <kethmi12345@gmail.com>
2024-09-09 12:26:34 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -1,7 +1,7 @@
-const { readEnv } = require('../lib/database');
-const { cmd, commands } = require('../command');
-const os = require("os");
-const { runtime } = require('../lib/functions');
+const {readEnv} = require('../lib/database')
+const {cmd, commands} = require('../command')
+const os = require("os")
+const {runtime} = require('../lib/functions')
 
 cmd({
     pattern: "alive",
@@ -9,8 +9,9 @@ cmd({
     desc: "Check uptime, RAM usage, and more",
     category: "main",
     filename: __filename
-}, async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+}, async (conn, mek, m, {from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
     try {
+
         const config = await readEnv();
 
         // RAM usage
@@ -18,15 +19,18 @@ cmd({
         const usedRAM = (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2); // Used RAM in MB
         const freeRAM = (totalRAM - parseFloat(usedRAM)).toFixed(2); // Free RAM in MB
 
+        // Check AUTO_STATUS_READ variable
+        const autoStatusRead = config.AUTO_STATUS_READ === 'true' ? 'Enabled' : 'Disabled';
+
         let status = `*✸𝕎𝔼𝕃ℂ𝕆𝕄𝔼 𝕋𝕆 ℤ𝔸𝕀ℝ𝕆 𝕄𝔻 𝔹𝕆𝕋✸*
 
-
-𝙽𝙾𝚂𝙴𝙳 𝚁𝙰𝙼: ${usedRAM} 𝙼𝙱 
-𝚄𝙿 𝚃𝙸𝙼𝙴: ${runtime(process.uptime())} 
-®𝚅𝙸𝙼𝙰𝙼𝙾𝙳𝚂®`;
+𝙽𝙾𝚆 𝚄𝚂𝙴𝙳 𝚁𝙰𝙼: ${usedRAM} 𝙼𝙱 
+𝚄𝙿 𝚃𝙸𝙼𝙴: ${runtime(process.uptime())}
+𝙰𝕌𝚃𝙾 𝚂𝚃𝙰𝚃𝚄𝚂 𝚁𝙴𝙰𝙳: ${autoStatusRead}
+®𝚅𝙸𝙼𝙰𝙼𝙾𝙳𝚂®`
 
         // URL of the image you want to include
-        const imageUrl = 'https://i.ibb.co/6mzcHsN/20240907-102239.jpg'; 
+        const imageUrl = 'https://i.ibb.co/6mzcHsN/20240907-102239.jpg'; // Replace with your actual image URL
 
         // Send the image with the status as the caption
         await conn.sendMessage(from, {
@@ -34,18 +38,8 @@ cmd({
             caption: status
         }, { quoted: mek || null });
 
-        // Auto-delete all messages in the chat after 10 seconds (10000 milliseconds)
-        setTimeout(async () => {
-            // Fetch the chat messages
-            const messages = await conn.fetchMessages(from, { delete: message.key });
-            for (const message of messages) {
-                if (message.key.fromMe) continue; // Skip messages sent by the bot
-                await conn.sendMessage(from, { delete: message.key });
-            }
-        }, 1000); // Adjust the time as needed
-
     } catch (e) {
-        console.log(e);
-        reply(`Error: ${e}`);
+        console.log(e)
+        reply(`Error: ${e}`)
     }
-});
+})
