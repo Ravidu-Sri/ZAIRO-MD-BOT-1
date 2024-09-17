45779979d82b2073e9e0913fbac2b259f9271887
vimamodz <125995272+vimamodz@users.noreply.github.com>
2024-09-17 17:27:55 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "G2QnxCgK#T5_HoMK5BFw5AMPam402CgzeCHxhkh73x2XvFDuW31U",
+SESSION_ID: process.env.SESSION_ID || "n2JVAARY#q0dAUVae8FnAEL9m_e_9VJN9yg8skKRNWufd_preu7A",
 MONGODB: process.env.MONGODB || "mongodb://mongo:UulqRzbarHKPlTmjsXTUnyCBstadpehV@junction.proxy.rlwy.net:59009",
 
 
