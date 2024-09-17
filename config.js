af2102db27422fe4dcb47afdcdbdb8afbe1b4a9f
vimanew <kethmi12345@gmail.com>
2024-09-17 12:16:28 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "G2QnxCgK#T5_HoMK5BFw5AMPam402CgzeCHxhkh73x2XvFDuW31U",
-MONGODB: process.env.MONGODB || "mongodb://mongo:RAERtaWSMaYqhXWHfcUdHPHKOFmoRokG@junction.proxy.rlwy.net:49687",
+MONGODB: process.env.MONGODB || "mongodb://mongo:UulqRzbarHKPlTmjsXTUnyCBstadpehV@junction.proxy.rlwy.net:59009",
 
 
 
